b289255813e25ffa79c6fd05d389b2d8eed2a1c8
miqueiasbelfort <kawasousa1554@gmail.com>
2023-01-01 19:51:52 -0300

    teste adição

---
+++ src/js/calculadora.js
@@ -0,0 +1,24 @@
+let Calculadora = {
+    adicionar: function(n1, n2){
+        if(isNaN(n1) || isNaN(n2)){
+            return 0
+        }
+
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
+        return n1 + n2
+    },
+    subtrair: function(n1,n2){
+        return 0
+    },
+    dividir: function(n1, n2){
+        return 0
+    },
+    multiplicar: function(n1, n2){
+        return 55
+    }
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+    module.exports = Calculadora
+}
